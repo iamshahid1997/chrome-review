ea3b0b1b7d2792561840081bdca93283f0593aef
Shahid Hassan Ansari <41151902+iamshahid1997@users.noreply.github.com>
2021-03-19 01:01:13 +0530

    Delete application.py

--- application.py
+++
@@ -1,7 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from website import create_app
-
-application = create_app()
-
-if __name__ == '__main__':
-    application.run(debug=True)
